492f4e3b8a77292cb54d0833be2b8c65f469d115
jhs512 <jangka512@gmail.com>
2021-03-14 17:36:44 +0900

    게시물 리스트 구현

--- app/src/main/java/com/example/sbs/myapplication/binding/BindingAdapters.java
+++ app/src/main/java/com/example/sbs/myapplication/binding/BindingAdapters.java
@@ -3,6 +3,7 @@ package com.example.sbs.myapplication.binding;
 import android.widget.ImageView;
 
 import androidx.databinding.BindingAdapter;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.sbs.myapplication.util.Util;
 
@@ -16,5 +17,10 @@ public class BindingAdapters {
     @BindingAdapter({"imgUrl", "borderRadius"})
     public static void imgUrl(ImageView view, String imgUrl, int borderRadius) {
         Util.loadImageOn(imgUrl, view, borderRadius);
+    }
+
+    @BindingAdapter({"adapter"})
+    public static void adapter(RecyclerView view, RecyclerView.Adapter adapter) {
+        view.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/sbs/myapplication/ui/HomeMainViewModel.java
+++ app/src/main/java/com/example/sbs/myapplication/ui/HomeMainViewModel.java
@@ -5,7 +5,6 @@ import androidx.lifecycle.ViewModel;
 
 import com.example.sbs.myapplication.dto.Article;
 import com.example.sbs.myapplication.service.ArticleService;
-import com.example.sbs.myapplication.util.Util;
 
 import javax.inject.Inject;
 
@@ -13,12 +12,15 @@ import dagger.hilt.android.lifecycle.HiltViewModel;
 
 @HiltViewModel
 public class HomeMainViewModel extends ViewModel {
+    public RecyclerViewAdapterArticle recyclerViewAdapterArticle;
     public MutableLiveData<Article> lvFirstArticle = new MutableLiveData<>();
 
     @Inject
     public HomeMainViewModel(ArticleService articleService) {
+        recyclerViewAdapterArticle = new RecyclerViewAdapterArticle();
+
         articleService.usr_article_list(1, 1, rb -> {
-            lvFirstArticle.setValue(rb.body.articles.get(0));
+            recyclerViewAdapterArticle.addArticles(rb.body.articles);
         });
     }
 }
